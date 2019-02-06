bf7749019831d0224edd86e63499315e5a1fb8e9
Adam Nelson <anelson@users.noreply.github.com>
2019-02-06 17:51:11 +0100

    Add current Rust bindings generated by bindgen

---
+++ libstapsdt-sys/src/lib.h
@@ -0,0 +1,10 @@
+#ifndef __LIB_H
+#define __LIB_H
+
+/* This header is used only as the input to bindgen to generate rust bindings for the
+ * libstapsdt library */
+
+#include <libstapsdt.h>
+
+#endif
+
