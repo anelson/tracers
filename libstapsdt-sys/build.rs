365d5039b2bb6d71489842999a027edd52cf3d7b
Adam Nelson <anelson@users.noreply.github.com>
2019-02-07 13:55:17 +0100

    Fix a build which actually wasn't working before; explicitly link with dependent static deps

--- libstapsdt-sys/build.rs
+++ libstapsdt-sys/build.rs
@@ -26,28 +26,22 @@ fn main() {
 
     // It's unlikely pkg_config knows about this, since the library's own deb package doesn't
     // register the library with pkg-config.  However it doesn't hurt to try.
-    if let Ok(pkg) = pkg_config::Config::new()
-        .statik(!wants_dynamic)
-        .probe("libstapsdt")
-    {
-        for path in &pkg.include_paths {
-            println!("cargo:include={}", path.display());
-        }
-
-        for path in &pkg.link_paths {
-            println!("cargo:rustc-link-search=native={}", path.display());
-        }
-
-        for lib in &pkg.libs {
-            println!("cargo:rustc-link-lib={}{}", statik, lib);
-        }
-
+    if try_pkgconfig("libstapsdt", wants_dynamic).is_ok() {
+        // This is an unlikely code path but pkg_config found the dependencies and printed
+        // them out for cargo to read already
         return;
     }
 
     // no matter what, tell cargo to link with this libstapd library, either the one that's
     // installed or the one we'll build below
     println!("cargo:rustc-link-lib={}{}", statik, "stapsdt");
+
+    // In the dynamic link case, dependencies are resolved at runtime, but in the static case dependencies
+    // must be resolved now.  That means we must also resolve the libstapsdt's dependencies libelf and libdl,
+    // and both of those must be static also.
+    if !wants_dynamic {
+        let _ = try_pkgconfig("libelf", false);
+    }
 
     //The makefile for libstapsdt is mercifully simple, and since it's wrapping a Linux-only
     //subsystem SytemTap there's no cross-platform nonsense either.
@@ -61,16 +55,19 @@ fn main() {
 
     let mut cfg = cc::Build::new();
     cfg.warnings(true)
-        .warnings_into_errors(true)
+        .warnings_into_errors(false) //it pains me to do this but the lib doesn't compile clean
         .static_flag(!wants_dynamic)
         .flag_if_supported("-std=gnu11")
-        .flag_if_supported("-Wl")
-        .flag("-lelf")
-        .flag("-ldl")
         .flag("-z")
         .flag("noexecstack")
         .pic(true)
         .out_dir(&build);
+
+    // The libelf-sys crate which wraps the libelf library may expose details about where to find
+    // its include files
+    if let Ok(elf_include) = env::var("DEP_ELF_INCLUDE") {
+        cfg.include(elf_include);
+    }
 
     // Accept overrides for include and lib paths
     if let Ok(include_dir) = env::var("LIBSTAPSDT_INCLUDE_DIR") {
@@ -107,12 +104,39 @@ fn main() {
     }
 
     cfg.files(
-        glob(src_path.join("src/*.h").to_str().unwrap())
+        glob(src_path.join("src/*.c").to_str().unwrap())
             .unwrap()
             .map(|x| x.unwrap()),
     )
     .file(src_path.join("src/asm/libstapsdt-x86_64.s"))
     .compile(&format!("libstapsdt.{}", libext));
+}
+
+fn try_pkgconfig(
+    package: &str,
+    wants_dynamic: bool,
+) -> Result<pkg_config::Library, pkg_config::Error> {
+    let pkg = pkg_config::Config::new()
+        .statik(!wants_dynamic)
+        .probe(package)?;
+    let cargo_link_lib = |lib: &str| {
+        let statik = if wants_dynamic { "" } else { "static=" };
+        println!("cargo:rustc-link-lib={}{}", statik, lib);
+    };
+
+    for path in &pkg.include_paths {
+        println!("cargo:include={}", path.display());
+    }
+
+    for path in &pkg.link_paths {
+        println!("cargo:rustc-link-search=native={}", path.display());
+    }
+
+    for lib in &pkg.libs {
+        cargo_link_lib(lib);
+    }
+
+    Ok(pkg)
 }
 
 fn libstapsdt_installed(cfg: &cc::Build) -> bool {
@@ -122,7 +146,10 @@ fn libstapsdt_installed(cfg: &cc::Build) -> bool {
     //Start with a copy of the cc config, but cc only supports building libraries
     //so at some point we'll have to override it
     let mut cfg = cfg.clone();
-    cfg.file("src/test/libstapsdt.c").flag("-lstapsdt");
+    cfg.file("src/test/libstapsdt.c")
+        .flag("-lstapsdt")
+        .flag("-lelf")
+        .flag("-ldl");
 
     let compiler = cfg.get_compiler();
     let mut cmd = compiler.to_command();
--- libstapsdt-sys/src/lib.rs
+++ libstapsdt-sys/src/lib.rs
@@ -8,20 +8,21 @@ include!("libstapsdt.rs");
 mod tests {
     use super::*;
 
+    use std::ffi::CString;
     use std::os::raw::*;
 
     #[test]
     fn smoke_test() {
         unsafe {
-            let provider = providerInit(CString::new("foo"));
+            let provider = providerInit(CString::new("foo").unwrap().as_ptr());
             assert!(!provider.is_null());
 
-            let probe1 = providerAddProbe(provider, "probe1", 0);
+            let probe1 = providerAddProbe(provider, CString::new("probe1").unwrap().as_ptr(), 0);
             assert!(probe1.is_null());
 
             let probe2 = providerAddProbe(
                 provider,
-                "probe2",
+                CString::new("probe2").unwrap().as_ptr(),
                 6,
                 ArgType_t_uint64,
                 ArgType_t_uint16,
@@ -33,8 +34,8 @@ mod tests {
             assert!(probe2.is_null());
 
             //Probes are not loaded yet, so they definitely should not be enabled
-            assert_eq!(0, providerIsEnabled(probe1));
-            assert_eq!(0, providerIsEnabled(probe2));
+            assert_eq!(0, probeIsEnabled(probe1));
+            assert_eq!(0, probeIsEnabled(probe2));
 
             assert_eq!(SDTError_t_noError, providerLoad(provider));
 
@@ -43,9 +44,9 @@ mod tests {
                 probeFire(
                     probe2,
                     6 as c_ulonglong,
-                    7 as c_ushort,
+                    7 as c_ushort as c_uint, // hurray weird C variadic conventions
                     8 as c_ulong,
-                    9 as c_uchar,
+                    9 as c_uchar as c_uint, // hurray weird C variadic conventions
                     -10 as c_longlong,
                     -11 as c_long,
                 );
