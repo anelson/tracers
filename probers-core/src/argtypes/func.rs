0c014c8f9ab4105ee6d1a66f0cfccb1f9c7ef526
Adam Nelson <anelson@users.noreply.github.com>
2019-02-13 17:36:44 +0100

    Refactor wrappers of arbitrary Fn and String types and start to notice they are similar and maybe should be refactored further...

--- probers-core/src/argtypes/func.rs
+++ probers-core/src/argtypes/func.rs
@@ -9,7 +9,7 @@ where
     type WrapperType = FuncProbeArgTypeWrapper<'a, F, T>;
 }
 
-pub struct FuncProbeArgTypeWrapper<'a, F, T>(&'a F)
+pub struct FuncProbeArgTypeWrapper<'a, F, T>(&'a F, Option<<T as ProbeArgType<T>>::WrapperType>)
 where
     F: Fn() -> T,
     T: ProbeArgType<T> + Debug;
@@ -22,12 +22,21 @@ where
     type CType = <<T as ProbeArgType<T>>::WrapperType as ProbeArgWrapper<T>>::CType;
 
     fn new(arg: &'a F) -> Self {
-        FuncProbeArgTypeWrapper(arg)
+        FuncProbeArgTypeWrapper(arg, None)
     }
 
     fn to_c_type(&mut self) -> Self::CType {
-        let arg = self.0();
-        let mut wrapped = super::wrap(arg);
+        //If the wrapper for the function's return value hasn't been created yet, create it
+        let func = self.0;
+
+        //It's not enough to just call the underlying wrapper's to_c_type().  In some cases
+        //(specifically with the StringWrapper for Rust strings), memory is allocated which is
+        //freed when the wrapper goes out of scope.  Thus we have to keep it around, hence the
+        //Option type.
+        let wrapped = self.1.get_or_insert_with(|| {
+            let arg = func();
+            super::wrap(arg)
+        });
         wrapped.to_c_type()
     }
 
--- probers-core/src/argtypes/string.rs
+++ probers-core/src/argtypes/string.rs
@@ -5,10 +5,7 @@ use std::os::raw::*;
 /// A wrapper around the Rust string slice type `&str`, which on demand converts the string slice
 /// into a null-terminated C-style string.
 #[derive(Debug)]
-pub struct StringWrapper<'a> {
-    string: &'a str,
-    c_string: Option<CString>,
-}
+pub struct StringWrapper<'a>(&'a str, Option<CString>);
 
 impl<'a> ProbeArgType<&'a str> for &'a str {
     type WrapperType = StringWrapper<'a>;
@@ -18,40 +15,31 @@ impl<'a> ProbeArgWrapper<&'a str> for StringWrapper<'a> {
     type CType = *const c_char;
 
     fn new(arg: &'a str) -> Self {
-        StringWrapper {
-            string: arg,
-            c_string: None,
-        }
+        StringWrapper(arg, None)
     }
 
     fn to_c_type(&mut self) -> Self::CType {
         // Create a CString from the string value, which can fail if there are embedded NUL bytes
         // in the string.  In that case we have a default string to fall back on
-        match &self.c_string {
-            None => match CString::new(self.string) {
+        let string = self.0;
+        let cstr = self.1.get_or_insert_with(|| {
+            match CString::new(string) {
                 Ok(cstr) => {
-                    //We are guaranteed that c_string will live as long as this struct does,
-                    //so we can safely return a naked pointer
-                    let c_ptr = cstr.as_ptr();
-                    self.c_string = Some(cstr);
-                    c_ptr
+                    //Unless `self.0` contains a NUL byte somewhere, this will succeed.
+                    cstr
                 }
                 Err(_) => {
                     //This error means there was an embedded NUL byte in the string which can't be properly
                     //represented in a C string.  Use our default instead
-                    //
-                    //This probably looks like it's not safe but the c_str! macro gives us a string literal compiled into
-                    //the binary so we can be guaranteed the memory never moves so this pointer is always valid
-                    c_str_macro::c_str!("<probers-core failed to create CString>").as_ptr()
+                    CString::new("<probers-core failed to create CString>")
+                        .expect("Failed to create static CString")
                 }
-            },
-            Some(cstr) => {
-                //If to_c_type is called twice that's a bug in the framework, but this code is needed
-                //to guard against undefined behavior in that event.  If the c string has already been computed,
-                //don't re-compute it as that means we have a dangling pointer returned from the first invocation of to_c_type
-                cstr.as_ptr()
             }
-        }
+        });
+
+        //Give the naked pointer to the CString.  This will be valid as long as this instance of the struct
+        //is in scope
+        cstr.as_ptr()
     }
 
     fn default_c_value() -> Self::CType {
