7b924cfa47a06925ac926ba020e2e67e29ed4af2
Adam Nelson <anelson@users.noreply.github.com>
2019-02-17 20:42:47 +0100

    Remove empty test modules from `lib.rs`

--- probers-core/src/lib.rs
+++ probers-core/src/lib.rs
@@ -6,26 +6,3 @@ extern crate quickcheck_macros;
 
 pub mod argtypes;
 pub use crate::argtypes::*;
-
-#[cfg(test)]
-mod tests {
-    use super::argtypes::*;
-
-    #[test]
-    fn it_works() {
-        let somearg = 5u64;
-        probe(somearg);
-
-        assert_eq!(2 + 2, 4);
-    }
-
-    fn probe<A: ProbeArgType<A>>(arg: A) -> () {
-        let mut wrapper = wrap(arg);
-        probe2(&mut wrapper);
-    }
-
-    fn probe2<W: ProbeArgWrapper>(arg: &mut W) -> () {
-        let _c_type = arg.as_c_type();
-        println!("C type is {:?}", arg);
-    }
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,1 @@
 #![deny(warnings)]
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
