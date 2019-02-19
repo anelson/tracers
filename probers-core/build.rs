1739cf88c8b1acfd72854474835311db15667465
Adam Nelson <anelson@users.noreply.github.com>
2019-02-19 18:50:57 +0100

    Implement quickcheck tests for the probe API types

--- probers-core/build.rs
+++ probers-core/build.rs
@@ -8,11 +8,14 @@ use std::io::Write;
 use std::path::Path;
 
 const MAX_ARITY: usize = 12; //AFAIK Rust itself only allows tuples up to this arity
+const MAX_QUICKCHECK_ARITY: usize = 8; //this is an unfortunate limit.
 
 fn main() -> Fallible<()> {
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("probe_args.rs");
+    let dest_tests_path = Path::new(&out_dir).join("probe_args_tests.rs");
     let mut f = File::create(&dest_path)?;
+    let mut f_tests = File::create(&dest_tests_path)?;
 
     for arity in 0..=MAX_ARITY {
         let type_params = get_type_param_names(arity);
@@ -20,6 +23,7 @@ fn main() -> Fallible<()> {
     }
 
     f.write_all(generate_unsafe_provider_probe_impl_trait().as_bytes())?;
+    f_tests.write_all(generate_tests().as_bytes())?;
 
     Ok(())
 }
@@ -78,7 +82,7 @@ fn generate_probe_args_impl(type_params: &Vec<String>) -> String {
             fn arg_types() -> Vec<CType> {{
                 vec![{ctypes}]
             }}
-            fn fire_probe<ImplT: UnsafeProviderProbeImpl>(self, probe: &ImplT) -> () {{
+            fn fire_probe<ImplT: UnsafeProviderProbeImpl>(self, probe: &mut ImplT) -> () {{
                 {wrapper_decls};
                unsafe {{ probe.fire{arg_count}({probe_args}) }}
             }}
@@ -124,7 +128,7 @@ fn generate_unsafe_provider_probe_impl_trait() -> String {
             /// access.  The Rust compiler should be able to inline this implementation for maxmimum performance.
             fn is_enabled(&self) -> bool;
 
-            unsafe fn fire0(&self) -> ();
+            unsafe fn fire0(&mut self) -> ();
     "#.to_string();
 
     for arity in 1..=MAX_ARITY {
@@ -134,7 +138,7 @@ fn generate_unsafe_provider_probe_impl_trait() -> String {
 
         decl += &format!(
             r##"
-            unsafe fn fire{arg_count}<{type_list}>(&self, {args}) -> ()
+            unsafe fn fire{arg_count}<{type_list}>(&mut self, {args}) -> ()
                 where {where_clause};
             "##,
             arg_count = type_params.len(),
@@ -149,6 +153,183 @@ fn generate_unsafe_provider_probe_impl_trait() -> String {
     }
 
     decl.push_str("}");
+
+    decl
+}
+
+/// This doesn't generate ALL test code, but it generates some test helpers that the `probes` module will use.
+fn generate_tests() -> String {
+    [
+        generate_test_unsafe_probe_impl(),
+        "\n".to_string(),
+        generate_probe_tests(),
+    ]
+    .concat()
+}
+
+/// Generates a test-only implementation of UnsafeProviderProbeImpl which passes its parameters to `sprintf` and
+/// exposes the resulting string as a Rust string type for verification
+fn generate_test_unsafe_probe_impl() -> String {
+    let mut decl = r#"
+
+    #[cfg(test)]
+    impl UnsafeProviderProbeImpl for TestingProviderProbeImpl {
+        fn is_enabled(&self) -> bool {
+            self.is_enabled
+        }
+
+        unsafe fn fire0(&mut self) -> () {
+            libc::snprintf(self.buffer.as_ptr() as *mut c_char, BUFFER_SIZE, self.format_string.as_ptr());
+            self.log_call();
+        }
+    "#
+    .to_string();
+
+    for arity in 1..=MAX_ARITY {
+        //For every possible arity level, declare a `probeN` method that takes a tuple of N
+        //native (meaning C native) types to pass to the probe.
+        let type_params = get_type_param_names(arity);
+
+        decl += &format!(
+            r##"
+            unsafe fn fire{arg_count}<{type_list}>(&mut self, {args}) -> ()
+                where {where_clause} {{
+                libc::snprintf(self.buffer.as_ptr() as *mut c_char,
+                    BUFFER_SIZE,
+                    self.format_string.as_ptr(),
+                    {probe_args});
+                self.log_call();
+            }}
+            "##,
+            arg_count = type_params.len(),
+            type_list = type_params.join(","),
+            args = xform_types_i(&type_params, |i, x| format!("arg{}: {}", i, x)).join(","),
+            where_clause = xform_types(&type_params, |x| format!(
+                "{t}: ProbeArgNativeType<{t}>",
+                t = x
+            ))
+            .join(","),
+            probe_args = if type_params.is_empty() {
+                "".to_string()
+            } else {
+                xform_types_i(&type_params, |i, _| format!("arg{}", i)).join(",")
+            }
+        );
+    }
+
+    decl.push_str("}");
+
+    decl
+}
+
+fn generate_probe_tests() -> String {
+    const ARG_TYPES: &[(&str, &str)] = &[
+        ("u64", "%u"),
+        ("u32", "%u"),
+        ("u16", "%u"),
+        ("u8", "%u"),
+        ("String", "%s"),
+        ("bool", "%u"),
+        ("i64", "%d"),
+        ("i32", "%d"),
+        ("i16", "%d"),
+        ("i8", "%d"),
+    ];
+
+    /// Pick an argument type for argument `n`.
+    ///
+    /// Returns  tuple containing:
+    /// * Name of the arg
+    /// * Rust data type of the arg
+    /// * C format string to use for the arg
+    /// * bool indicating if this arg needs to be passed by reference to the probe
+    fn choose_arg_for_n(n: usize) -> (String, String, String, bool) {
+        let (type_name, format_specifier) = ARG_TYPES[n % ARG_TYPES.len()];
+
+        let byref = type_name == "String" || type_name.starts_with("Option");
+
+        (
+            format!("arg{}", n),
+            type_name.to_string(),
+            format_specifier.to_string(),
+            byref,
+        )
+    }
+
+    let mut decl = "".to_string();
+
+    for arity in 1..=MAX_QUICKCHECK_ARITY {
+        //For every possible arity level, write some quickcheck tests that take a tuple and exercise
+        //the probe firing behavior
+        let type_params = get_type_param_names(arity);
+
+        let args = (1..=arity)
+            .map(choose_arg_for_n)
+            .collect::<Vec<(String, String, String, bool)>>();
+
+        let args_declaration: Vec<String> = args
+            .iter()
+            .map(|(name, typ, _, _)| format!("{}: {}", name, typ))
+            .collect();
+
+        let expected_arg_values: Vec<String> = args
+            .iter()
+            .map(|(name, typ, _, _)| {
+                if typ == "String" {
+                    format!("c_and_back_again(&{})", name)
+                } else if typ == "bool" {
+                    format!("u8::from({})", name)
+                } else {
+                    name.to_string()
+                }
+            })
+            .collect();
+
+        let args_tuple = make_tuple_type(
+            &args
+                .iter()
+                .map(|(name, _, _, byref)| {
+                    if *byref {
+                        format!("&{}", name)
+                    } else {
+                        format!("{}", name)
+                    }
+                })
+                .collect(),
+        );
+
+        let c_format_string = args
+            .iter()
+            .map(|(_, _, fmt, _)| fmt.to_string())
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        let rust_format_string = std::iter::repeat("{}".to_string())
+            .take(arity)
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        decl += &format!(
+            r##"
+            #[quickcheck]
+            fn test_fire{arg_count}({args_declaration}) -> bool {{
+                let mut probe_impl = make_test_probe("{c_format_string}".to_string());
+                let probe_args={args_tuple};
+                probe_impl.fire(probe_args);
+
+                assert_eq!(probe_impl.unsafe_probe_impl.calls,
+                    vec![format!("{rust_format_string}", {expected_arg_values})]);
+                true
+            }}
+            "##,
+            arg_count = type_params.len(),
+            args_declaration = args_declaration.join(", "),
+            c_format_string = c_format_string,
+            args_tuple = args_tuple,
+            rust_format_string = rust_format_string,
+            expected_arg_values = expected_arg_values.join(", ")
+        );
+    }
 
     decl
 }
