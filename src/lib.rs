d2accd29e8faaead482316605c4134b635f27a40
Adam Nelson <anelson@users.noreply.github.com>
2019-03-03 11:44:29 +0100

    Add type aliases for the selected tracer and make stap default

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
 #![deny(warnings)]
+
+pub use probers_core::{Tracer,Provider,ProviderProbe,ProviderBuilder};
+
+pub type SystemTracer = probers_stap::StapTracer;
+pub type SystemProvider = <SystemTracer as Tracer>::ProviderType;
+pub type SystemProbe = <SystemTracer as Tracer>::ProbeType;
