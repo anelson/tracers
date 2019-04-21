f3b8436870a088f215e5b310a33dc01e36645fd8
Adam Nelson <anelson@users.noreply.github.com>
2019-04-21 12:11:19 +0200

    Implement `probers-build` more elegantly using the `Cargo.toml` manifest

--- probers-build/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! There is no actual code for `probers-build`.  It's an exactly duplicate of `probers-codegen`,
-//! but Cargo will treat it as a separate crate so it can be used as a `build-dependency` while
-//! `probers-codegen` is a regular dependency.  That allows us to work around the nasty cargo bug
-//! [#4866](https://github.com/rust-lang/cargo/issues/4866).
-
-include!("../../probers-codegen/lib.rs")
-
